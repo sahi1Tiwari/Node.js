f0e13b1d61bc43983fabef096dd706b53dfa7b5e
Sahil Tiwari <sahiltiwari0415@gmail.com>
2021-11-15 23:03:39 +0530

    files modified

---
+++ fileSystem.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+// first statement to write some text in a file
+// fs.writeFileSync('read.txt', 'Hii, there this is SAHIL')
+
+fs.writeFileSync('read.txt','Yoo vro this is Vishwanath')
+// after using the previous line in the read.txt the previous content is deleted
---
+++ play.js
@@ -0,0 +1,2 @@
+// Hii this is Sahil Tiwari and this is my first code of NODE.JS
+console.log("Hii This is Sahil")
