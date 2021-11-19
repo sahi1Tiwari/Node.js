ee9c86cd772d85c5ef45c3e370b21460205c4524
sahi1Tiwari <sahiltiwari0415@gmail.com>
2021-11-19 16:21:05 +0530

    file modified

--- main.js
+++ main.js
@@ -11,4 +11,17 @@ const fs = require('fs');
 // });
 
 // -> To append the file bio.txt .
-fs.appendFile('./sahil/bio.txt',"My name is Sahil Tiwari and i Have created this file using node.js .", (err)=> console.log("File is appended."));
+// fs.appendFile('./sahil/bio.txt',"My name is Sahil Tiwari and i Have created this file using node.js .", (err)=> console.log("File is appended."));
+
+// -> Reading a file.
+// fs.readFile('./sahil/bio.txt', 'utf-8', (err,data)=>console.log(data));
+// here we use 'utf-8' because node.js reads the data in the form of buffe or chunks
+
+// -> for renaming a file
+// fs.rename('./biodata.txt','./sahil/bio.txt',(err)=>console.log('File is renamed.'))
+
+// -> for deleting a file
+// fs.unlink('./sahil/bio.txt', (err)=>console.log('File deleted'));
+
+// -> for deleting a folder
+// fs.rmdir('./sahil',(err)=>console.log('Folder deleted'));
